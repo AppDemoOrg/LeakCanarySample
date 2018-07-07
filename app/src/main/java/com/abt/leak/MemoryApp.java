37f66d88b2ee07fa104faee75f63a46683074514
huangweiqi <askviky2010@gmail.com>
2018-07-07 17:26:21 +0800

    后去最大内存

--- app/src/main/java/com/abt/leak/MemoryApp.java
+++ app/src/main/java/com/abt/leak/MemoryApp.java
@@ -2,15 +2,22 @@ package com.abt.leak;
 
 import android.app.Application;
 import android.os.StrictMode;
+import android.util.Log;
 
 import com.squareup.leakcanary.LeakCanary;
 
-public class LeakApp extends Application {
+public class MemoryApp extends Application {
 
+    private static final String TAG = MemoryApp.class.getSimpleName();
     @Override
     public void onCreate() {
         super.onCreate();
         setupLeakCanary();
+
+        Runtime rt=Runtime.getRuntime();
+        long maxMemory=rt.maxMemory();
+        String max=Long.toString(maxMemory/(1024*1024));
+        Log.i(TAG,"maxMemory:"+max);
     }
 
     protected void setupLeakCanary() {
--- init_repo.bash
+++ init_repo.bash
@@ -6,8 +6,8 @@ echo 'git add -A'
 git add -A
 echo 'git commit -m "init repo"'
 git commit -m "init repo"
-echo 'git remote add origin https://github.com/AppDemoOrg/LeakCanarySample.git'
-git remote add origin https://github.com/AppDemoOrg/LeakCanarySample.git
+echo 'git remote add origin https://github.com/AppDemoOrg/MemorySample.git'
+git remote add origin https://github.com/AppDemoOrg/MemorySample.git
 echo 'git push -u origin master'
 git push -u origin master
 # git bash DONE
